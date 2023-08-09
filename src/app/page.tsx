f24e06dfb4950e6cf438a9bb808fdb185471a7a2
jinseok <wlstjrghdud@naver.com>
2023-08-10 00:23:39 +0900

    feat: tab ui 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+'use client'
 import React from 'react'
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 export default function Home() {
   return (
     <>
-      <div className=" body1">asdfdsfasd</div>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+          <Tab>Title 2</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 4</h2>
+        </TabPanel>
+      </Tabs>
     </>
   )
 }
