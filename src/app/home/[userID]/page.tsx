9ff5f65d2833307a4245f45b9bf71e3e2e131a25
jinseok <wlstjrghdud@naver.com>
2023-09-21 01:08:57 +0900

    chore: 빌드 에러 수정

--- src/app/home/[userID]/page.tsx
+++ src/app/home/[userID]/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/home/Header'
 
 import React from 'react'
 
-export default async function Home({ parmas }: { params: { userID: string } }) {
+export default async function Home({}: { params: { userID: string } }) {
   return (
     <div className="container">
       <Header />
