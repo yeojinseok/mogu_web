7c6d6a003b4aa085297e2ecf70481704d5c4b423
jinseok <wlstjrghdud@naver.com>
2023-09-29 16:03:19 +0900

    docs: moguselect 버그 주석 추가

--- src/components/common/MoguSelect.tsx
+++ src/components/common/MoguSelect.tsx
@@ -7,6 +7,9 @@ type OptionType = {
   label: string
 }[]
 
+/**
+ * 최대 사이즈가 max-width를 넘어가는 경우 left값이 의도된 위치로 가지 않음
+ */
 export default function MoguSelect({
   options,
   onChange,
@@ -22,53 +25,64 @@ export default function MoguSelect({
     if (!iconRef.current) return
 
     const { left, bottom } = iconRef.current.getBoundingClientRect()
-
     return {
       right: left - 20,
       top: bottom + 10,
     }
   }
 
+  // useEffect(() => {
+  //   addEventListener('mousedown', e => console.log(e.clientX, e.clientY))
+  // }, [])
+
   return (
     <div
-      ref={iconRef}
-      tabIndex={-1}
-      className="flex "
-      onBlur={() => {
-        setIsFocus(false)
-      }}
+      style={
+        {
+          // transform: `rotate(0)`,
+        }
+      }
     >
       <div
-        onClick={() => {
-          setIsFocus(prev => !prev)
+        ref={iconRef}
+        tabIndex={-1}
+        className="flex bg-lime-300 "
+        onBlur={() => {
+          setIsFocus(false)
         }}
       >
-        <FaChevronDown size={12} />
-      </div>
-      {isFocus && (
         <div
-          className="fixed z-50 px-8 py-4 bg-white shadow-lg text-grey-500 rounded-8 min-w-120 "
-          style={getContainerRect()}
+          onClick={() => {
+            setIsFocus(prev => !prev)
+          }}
         >
-          {options.map((option, index) => (
-            <div
-              key={option.value}
-              css={
-                options.length !== index + 1
-                  ? tw` body_large border-b-1 border-b-grey-50`
-                  : undefined
-              }
-              className="py-8 text-center body_large"
-              onClick={() => {
-                setIsFocus(false)
-                onChange(option.value)
-              }}
-            >
-              {option.label}
-            </div>
-          ))}
+          <FaChevronDown size={12} />
         </div>
-      )}
+        {isFocus && (
+          <div
+            className="fixed z-50 px-8 py-4 bg-white shadow-lg text-grey-500 rounded-8 min-w-120 "
+            style={getContainerRect()}
+          >
+            {options.map((option, index) => (
+              <div
+                key={option.value}
+                css={
+                  options.length !== index + 1
+                    ? tw` body_large border-b-1 border-b-grey-50`
+                    : undefined
+                }
+                className="py-8 text-center body_large"
+                onClick={() => {
+                  setIsFocus(false)
+                  onChange(option.value)
+                }}
+              >
+                {option.label}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
