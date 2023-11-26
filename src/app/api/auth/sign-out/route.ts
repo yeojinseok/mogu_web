df88f27b2f78362d6b2d14ac92ec01cb2aa1b581
jinseok <wlstjrghdud@naver.com>
2023-11-27 01:08:10 +0900

    chore: 빌드 에러 수정

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
+  //@ts-ignore
   cookies().delete('refreshToken')
 
   redirect('/auth/email')
