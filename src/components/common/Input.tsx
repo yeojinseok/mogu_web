0ed05e7cc90f1ed1b21e0bba6e75427aa53b6713
jinseok <wlstjrghdud@naver.com>
2023-11-26 14:13:55 +0900

    design: Input 컴포넌트 error Text 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -5,16 +5,21 @@ import tw, { styled } from 'twin.macro'
 
 interface InputProps {
   showLabel: boolean
+  isError: boolean
 }
 
 const InputStyled = styled.input<InputProps>`
-  ${tw`w-full px-16 py-12 border-1 rounded-12 title_subsection h-78 border-grey-200 focus:outline-focus_green`}
+  ${tw`w-full px-16 py-12 border-1 rounded-12 title_subsection h-78`}
   ${props => (props.showLabel ? tw`pt-38` : undefined)}
+  ${props =>
+    props.isError
+      ? tw` border-error focus:outline-error`
+      : tw` border-grey-200 focus:outline-focus_green`}
 `
 
 export const Input = React.forwardRef<
   HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
+  React.InputHTMLAttributes<HTMLInputElement> & { errorText?: string }
 >(function Input(props, ref) {
   const [isFocus, setIsFocus] = useState(false)
   const [value, setValue] = useState('')
@@ -41,12 +46,20 @@ export const Input = React.forwardRef<
           setIsFocus(false)
         }}
         showLabel={showLabel}
+        isError={!!props.errorText}
       />
       {(isFocus || value.length > 0) && (
-        <div className="absolute text-focus_green top-12 left-16 body_default text-">
+        <div
+          css={
+            !!props.errorText
+              ? tw`absolute text-error top-12 left-16 body_default`
+              : tw`absolute text-focus_green top-12 left-16 body_default`
+          }
+        >
           {props.placeholder}
         </div>
       )}
+      <div className="pt-4 body_default text-error">{props.errorText}</div>
     </div>
   )
 })
